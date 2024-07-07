79ac4f2099087bff052c53476c0484c5bcea143c
Paulo-21 <paul.cibier@gmail.com>
2024-07-07 20:26:19 +0200

    first

--- src/job.rs
+++ src/job.rs
@@ -1,3 +1,23 @@
-pub async fn job() {
+use tokio::{io, net::TcpSocket};
 
+pub async fn job(ip : u32) -> io::Result<()> {
+    let one = (ip >> 24) & 255;
+    let two = (ip >> 16 ) & 255;
+    let tree = (ip >> 8) & 255;
+    let four = ip & 255;
+    let mut ip = String::with_capacity(26);
+    ip.push_str(one.to_string().as_str());
+    ip.push('.');
+    ip.push_str(two.to_string().as_str());
+    ip.push('.');
+    ip.push_str(tree.to_string().as_str());
+    ip.push('.');
+    ip.push_str(four.to_string().as_str());
+    ip.push_str(":8080");
+    //println!("{} {} {} {}", one, two, tree, four);
+    let addr = ip.parse().unwrap();
+    let socket = TcpSocket::new_v4()?;
+    let stream = socket.connect(addr).await?;
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
 use job::job;
+use tokio::{task::JoinSet, time::{sleep, Duration}};
 
 mod job;
 
-#[tokio::main]
+#[tokio::main(flavor = "multi_thread", worker_threads = 4)]
 async fn main() {
     // This is running on a core thread.
+    let mut set = JoinSet::new();
     
-    let blocking_task = tokio::task::spawn_blocking(|| {
-        // This is running on a blocking thread.
-        // Blocking here is ok.
-        let _ = job();
-    });
+    for i in 0..u32::MAX {
+        let k = i;
+        let _ =  set.spawn(async move {
+            let _ = job(k).await;
+        });
+    }
+    //sleep(Duration::from_secs(10)).await;
 
+    while let Some(res) = set.join_next().await {
+        let e = res.unwrap();
+    }
     // We can wait for the blocking task like this:
     // If the blocking task panics, the unwrap below will propagate the
     // panic.
-    blocking_task.await.unwrap();
 }
