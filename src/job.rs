d217dad71d508bc510a3bd67536a1bb24ae0e175
paul <paul.cibier@gmail.com>
2024-07-09 09:04:10 +0000

    even better

--- src/job.rs
+++ src/job.rs
@@ -1,17 +1,37 @@
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
+use tokio::stream;
 use tokio::{io, net::TcpSocket};
+use tokio::time::{timeout, Duration};
 
 pub async fn job(ip : u32) -> io::Result<()> {
     let one = (ip >> 24) as u8 & 255 ;
+    //let one = 86;
     let two = (ip >> 16 ) as u8 & 255;
     let tree = (ip >> 8) as u8 & 255;
     let four = (ip & 255) as u8;
     //println!("{} {} {} {}", one, two, tree, four);
-    let socket = TcpSocket::new_v4()?;
+    
     let ip = IpAddr::V4(Ipv4Addr::new(one, two, tree, four));
-    let addr = SocketAddr::new(ip, 8080);
-    let stream = socket.connect(addr).await?;
+    let addr = SocketAddr::new(ip, 22);
+    let socket = TcpSocket::new_v4()?;
+    let one_sec = Duration::from_secs(2);
+    socket.set_reuseport(true)?;
+    match timeout(one_sec, socket.connect(addr)).await {
+        Ok(res) => {
+            match res {
+                Ok(_stream) => {
+                    println!("CONNECTED {:?}", addr);
+                },
+                Err(err) => {
+                    //println!("err {}", err);
+                }
+            }
+        },
+        Err(err) => {
+            //println!("TO : {}", err);
+        }
+    }
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,34 @@
 use job::job;
-use tokio::{task::JoinSet, time::{sleep, Duration}};
+use sysinfo::{MemoryRefreshKind, System};
+use tokio::{task::JoinSet, time::{sleep, timeout, Duration, Instant}};
 
 mod job;
 
-#[tokio::main(flavor = "multi_thread", worker_threads = 4)]
+#[tokio::main(flavor = "multi_thread")]
+//#[tokio::main]
 async fn main() {
     // This is running on a core thread.
     let mut set = JoinSet::new();
-    
-    for i in 0..100000000 {
+    let mut sys = System::new_all();
+    let mut i = 1;
+
+    while i < u32::MAX {
         let k = i;
-        let _ =  set.spawn(async move {
-            let _ = job(k).await;
-        });
+        //if i%100 == 0 {
+            sys.refresh_memory_specifics(MemoryRefreshKind::new().with_ram());
+            //}
+            if sys.free_memory() > 1000000 {
+                let _ =  set.spawn(async move {
+                    let _ = job(k).await;
+                });
+                i+=1;
+            }
+            else {
+                println!("SLEEP");
+                sleep(Duration::from_secs(1)).await;
+                println!("WAKEUP");
+            }
     }
-    //sleep(Duration::from_secs(10)).await;
 
     while let Some(res) = set.join_next().await {
         let _ = res.unwrap();
