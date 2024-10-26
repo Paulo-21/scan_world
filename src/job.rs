62b532cdd9cba40f8232706594107572c6039769
Paulo-21 <paul.cibier@gmail.com>
2024-10-26 13:09:44 +0200

    new

--- src/job.rs
+++ src/job.rs
@@ -1,37 +1,34 @@
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-
-use tokio::stream;
-use tokio::{io, net::TcpSocket};
+use tokio::net::TcpSocket;
 use tokio::time::{timeout, Duration};
 
-pub async fn job(ip : u32) -> io::Result<()> {
-    let one = (ip >> 24) as u8 & 255 ;
+pub async fn job(ip : u32) {
+    /*let one = (ip >> 24) as u8 & 255 ;
     //let one = 86;
     let two = (ip >> 16 ) as u8 & 255;
     let tree = (ip >> 8) as u8 & 255;
-    let four = (ip & 255) as u8;
+    let four = (ip & 255) as u8;*/
     //println!("{} {} {} {}", one, two, tree, four);
     
-    let ip = IpAddr::V4(Ipv4Addr::new(one, two, tree, four));
+    //let ip = IpAddr::V4(Ipv4Addr::new(one, two, tree, four));
+    let ip = IpAddr::V4(Ipv4Addr::from(ip));
     let addr = SocketAddr::new(ip, 22);
-    let socket = TcpSocket::new_v4()?;
-    let one_sec = Duration::from_secs(2);
-    socket.set_reuseport(true)?;
+    let socket = TcpSocket::new_v4().unwrap();
+    let one_sec = Duration::from_secs(1);
+    socket.set_reuseport(true).unwrap();
     match timeout(one_sec, socket.connect(addr)).await {
         Ok(res) => {
             match res {
                 Ok(_stream) => {
                     println!("CONNECTED {:?}", addr);
                 },
-                Err(err) => {
+                Err(_err) => {
                     //println!("err {}", err);
                 }
             }
         },
-        Err(err) => {
+        Err(_err) => {
             //println!("TO : {}", err);
         }
     }
-
-    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use job::job;
 use sysinfo::{MemoryRefreshKind, System};
-use tokio::{task::JoinSet, time::{sleep, timeout, Duration, Instant}};
+use tokio::{time::{sleep, Duration, Instant}};
+use std::sync::{atomic::{AtomicU32, Ordering}, Arc};
 
 mod job;
 
@@ -8,32 +9,49 @@ mod job;
 //#[tokio::main]
 async fn main() {
     // This is running on a core thread.
-    let mut set = JoinSet::new();
     let mut sys = System::new_all();
-    let mut i = 1;
+    let mut sys2 = System::new_all();
+    let mut i_org : u32 = 1;
+    //let raw = &i_org as *const u32;
+    let shared = Arc::new(AtomicU32::new(1));
+    let s = shared.clone();
 
-    while i < u32::MAX {
-        let k = i;
-        //if i%100 == 0 {
-            sys.refresh_memory_specifics(MemoryRefreshKind::new().with_ram());
-            //}
-            if sys.free_memory() > 1000000 {
-                let _ =  set.spawn(async move {
-                    let _ = job(k).await;
-                });
-                i+=1;
+    let handle = tokio::runtime::Handle::current();
+    let runtime_monitor = tokio_metrics::RuntimeMonitor::new(&handle);
+    let limit_memory =  (sys.total_memory() as f64 * 0.92 ) as u64;
+    // print runtime metrics every 500ms
+    let frequency = std::time::Duration::from_millis(500);
+    /*tokio::spawn(async move {
+        let start = Instant::now();
+        let pr = s;
+        for metrics in runtime_monitor.intervals() {
+            sys2.refresh_memory_specifics(MemoryRefreshKind::new().with_ram());
+            println!("------------------------------------------");
+            println!("Metrics = {:?}", metrics);
+            println!("Tot {}, used : {}", sys2.total_memory(), sys2.used_memory());
+            let value = pr.load(Ordering::Relaxed);
+            println!(" {} / {} : {} % ", value , u32::MAX, value as f64 / u32::MAX as f64);
+            if start.elapsed().as_secs() > 0 {
+                println!("{} r/s", value as u64 / start.elapsed().as_secs());
             }
-            else {
-                println!("SLEEP");
-                sleep(Duration::from_secs(1)).await;
-                println!("WAKEUP");
-            }
+            tokio::time::sleep(frequency).await;
+        }
+    });*/
+    while i_org < u32::MAX {
+        let k = i_org;
+        sys.refresh_memory_specifics(MemoryRefreshKind::new().with_ram());
+        if sys.used_memory() < limit_memory {
+            tokio::task::spawn(async move {
+                let _ = job(k).await;
+            });
+            i_org +=1;
+            shared.store(i_org, Ordering::Relaxed);
+        }
+        else {
+            println!("SLEEP");
+            sleep(Duration::from_millis(500)).await;
+            println!("WAKEUP");
+        }
     }
 
-    while let Some(res) = set.join_next().await {
-        let _ = res.unwrap();
-    }
-    // We can wait for the blocking task like this:
-    // If the blocking task panics, the unwrap below will propagate the
-    // panic.
 }
